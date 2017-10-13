099cf2ee1de0f93215d8811233c6c033cb8b0278
Darryl Sokoloski <dsokoloski@clearfoundation.com>
2017-10-12 22:47:45 -0400

    Cosmetic clean-up.

--- deploy/exec-pre.sh
+++ deploy/exec-pre.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Set Netify Daemon command-line options
+# Set Netify Daemon command-line options:
 # At least one -I, --internal and/or -E, --external parameters are required.
 # See man netifyd(8) for all options.
 
-NETIFYD_OPTS=
 # NETIFYD_OPTS="--external eth0 --internal eth1"
+NETIFYD_OPTS=
 
 systemctl set-environment NETIFYD_OPTS="$NETIFYD_OPTS"
 
