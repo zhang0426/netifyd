6b5ac9cd539c94c7823e87a5c4b9916260bb3aea
Darryl Sokoloski <dsokoloski@clearfoundation.com>
2018-01-12 15:40:55 -0500

    Fixed user-agent string.

--- src/nd-upload.cpp
+++ src/nd-upload.cpp
@@ -277,7 +277,7 @@ void ndUploadThread::CreateHeaders(void)
 
     ostringstream user_agent;
     user_agent << "User-Agent: " <<
-        PACKAGE_NAME << "/" << GIT_RELEASE << "(" << _ND_CANONICAL_HOST << ")" <<
+        PACKAGE_NAME << "/" << GIT_RELEASE << " (" << _ND_CANONICAL_HOST << ")" <<
         " JSON/" << fixed << showpoint << setprecision(2) << ND_JSON_VERSION <<
         " nDPI/" << ndpi_revision();
 
