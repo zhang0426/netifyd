524896d4b58ffa560b70ede5c67caebc68f37ee9
Darryl Sokoloski <darryl@sokoloski.ca>
2018-03-08 22:33:09 -0500

    Removed nDPI-specific stuff from autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,9 @@
-#!/bin/sh
-
-# Autogen bits from ndpi/autogen.sh
-NDPI_MAJOR="2"
-NDPI_MINOR="1"
-NDPI_PATCH="0"
-NDPI_VERSION_SHORT="$NDPI_MAJOR.$NDPI_MINOR.$NDPI_PATCH"
-
-cat ndpi/configure.seed | sed "s/@NDPI_MAJOR@/$NDPI_MAJOR/g" | sed "s/@NDPI_MINOR@/$NDPI_MINOR/g" | sed "s/@NDPI_PATCH@/$NDPI_PATCH/g" | sed "s/@NDPI_VERSION_SHORT@/$NDPI_VERSION_SHORT/g" > ndpi/configure.ac
+#!/bin/sh -x
 
 # Regenerate configuration files
+(cd ./ndpi/ && ./autogen.sh)
 find $(pwd) -name configure.ac | xargs touch
 mkdir -vp m4 inih/m4 || exit 1
+
 autoreconf -i --force -I m4 || exit 1
 
