a59b527a879248967b24b24d3f906fede8443335
Darryl Sokoloski <dsokoloski@clearfoundation.com>
2018-07-10 13:53:18 -0400

    Added new environment file for systemd.

---
+++ deploy/env.sh
@@ -0,0 +1,6 @@
+# Netify Agent Environment Variables
+# Currently only sourced when started via systemd.
+
+# TCMALLOC Heap Profiler
+#HEAPPROFILE=/var/lib/netifyd/netifyd.hprof
+#HEAP_PROFILE_TIME_INTERVAL=60
