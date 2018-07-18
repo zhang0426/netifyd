52014b90367053bd31e90d46e58eacb2f6d015cc
Darryl Sokoloski <darryl@sokoloski.ca>
2018-07-17 22:01:06 -0400

    Do not overwrite configuration files on install.

--- deploy/install-edgeos.sh
+++ deploy/install-edgeos.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-sudo cp -r etc/* /etc/
+sudo cp -r etc/netify.d/* /etc/netify.d/
+if [ ! -f /etc/netifyd.conf ]; then
+    sudo cp etc/netifyd.conf /etc/
+fi
+if [ ! -f /etc/default/netifyd ]; then
+    sudo cp etc/default/netifyd /etc/default/
+fi
 sudo cp -r run/* /run/
 sudo cp -r usr/* /usr/
 sudo cp -r var/* /var/
