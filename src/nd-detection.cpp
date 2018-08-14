aaa86e2b558fda7dda0c6db323c658575c6c61e8
Darryl Sokoloski <darryl@sokoloski.ca>
2018-08-14 12:54:43 -0400

    Added interface internal boolean to flow JSON header.

--- src/nd-detection.cpp
+++ src/nd-detection.cpp
@@ -1174,6 +1174,7 @@ void ndDetectionThread::ProcessPacket(void)
             json.AddObject(NULL, "type", "flow");
             json.AddObject(NULL, "version", (double)ND_JSON_VERSION);
             json.AddObject(NULL, "interface", tag);
+            json.AddObject(NULL, "internal", internal);
             json_object *json_flow = new_flow->json_encode(
                 tag.c_str(), json, ndpi, false);
             json.AddObject(NULL, "flow", json_flow);
