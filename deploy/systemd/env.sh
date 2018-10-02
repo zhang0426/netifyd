7a84dc945fe4ccc1db6d26636a435968dce4b1e3
Darryl Sokoloski <darryl@sokoloski.ca>
2018-10-01 20:46:32 -0400

    Updated example TCMalloc Heap Profiler settings.

--- deploy/systemd/env.sh
+++ deploy/systemd/env.sh
@@ -6,5 +6,5 @@
 # Currently only sourced when started via systemd(1).
 
 # TCMALLOC Heap Profiler
-#HEAPPROFILE=/var/lib/netifyd/netifyd.hprof
-#HEAP_PROFILE_TIME_INTERVAL=60
+#HEAPPROFILE=/var/run/netifyd/netifyd.hprof
+#HEAP_PROFILE_TIME_INTERVAL=1800
