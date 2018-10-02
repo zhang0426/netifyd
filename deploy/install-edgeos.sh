86598ca62895a0cee4654303535975c88cbbc5da
Darryl Sokoloski <darryl@sokoloski.ca>
2018-10-02 14:53:52 -0400

    Updated EdgeOS install script.

--- deploy/install-edgeos.sh
+++ deploy/install-edgeos.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Manual install script for Ubiquiti EdgeOS (USG-3P)
 
 sudo cp -r etc/netify.d/* /etc/netify.d/
 if [ ! -f /etc/netifyd.conf ]; then
@@ -8,10 +9,12 @@ if [ ! -f /etc/default/netifyd ]; then
     sudo cp etc/default/netifyd /etc/default/
 fi
 sudo cp -r usr/* /usr/
-sudo cp -r var/* /var/
+sudo mkdir -p /var/run/netifyd
+sudo rm -rf /var/lib/netifyd
 
 sudo cp etc/cron.d/* /etc/cron.d/
 sudo /etc/init.d/cron restart
 
-sudo /usr/sbin/netifyd --provision
+sudo cp etc/init.d/* /etc/init.d/
 
+sudo /usr/sbin/netifyd --provision
