abfbfea7e6c2040e883e7f7f6525484f15081b89
Darryl Sokoloski <darryl@sokoloski.ca>
2019-04-04 15:50:05 -0400

    Moved custom signals to dedicated include file.

---
+++ include/nd-signal.h
@@ -0,0 +1,24 @@
+// Netify Agent
+// Copyright (C) 2015-2019 eGloo Incorporated <http://www.egloo.ca>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _ND_SIGNAL_H
+#define _ND_SIGNAL_H
+
+#define ND_SIG_UPDATE      (SIGRTMIN + 0)
+#define ND_SIG_SINK_REPLY  (SIGRTMIN + 1)
+
+#endif // _ND_SIGNAL_H
+// vi: expandtab shiftwidth=4 softtabstop=4 tabstop=4
