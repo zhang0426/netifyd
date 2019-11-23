041ee63aa409b9de7823fee0e74fc2c73f0eff42
Darryl Sokoloski <darryl@sokoloski.ca>
2019-11-22 23:11:33 -0500

    Added sink status to provision page.

--- deploy/pfsense/files/usr/local/www/netify/netify_provision.php
+++ deploy/pfsense/files/usr/local/www/netify/netify_provision.php
@@ -3,20 +3,33 @@ require_once('guiconfig.inc');
 require_once('/usr/local/pkg/netify/netify.inc');
 
 if ($_POST['status'] == 'update') {
-	$status = array('uuid' => '00-00-00-00');
-	$status = array('provisioned' => false);
+    $status = array('sink_enabled' => netifyd_sink_enabled());
 
-	$response = json_encode($status);
-	header('Content-Type: application/json');
-	header('Content-Length: ' . strlen($response));
+    $response = json_encode($status);
+    header('Content-Type: application/json');
+    header('Content-Length: ' . strlen($response));
 
-	echo json_encode($status);
+    echo json_encode($status);
 
-	exit;
+    exit;
+}
+else if (array_key_exists('sinkEnable', $_POST)) {
+    $enable = true;
+    if ($_POST['sinkEnable'] == 'false') $enable = false;
+    $status = array('result' => netifyd_enable_sink($enable));
+
+    $response = json_encode($status);
+    header('Content-Type: application/json');
+    header('Content-Length: ' . strlen($response));
+
+    echo json_encode($status);
+
+    exit;
 }
 
-include('head.inc');
 $pgtitle = array(gettext('Services'), gettext('Netify'), gettext('Provision'));
+
+include('head.inc');
 
 $tab_array = array();
 $tab_array[] = array(gettext('Status'), false, '/netify/netify_status.php');
@@ -30,81 +43,150 @@ $agent_status_url = netifyd_get_agent_status_url();
 ?>
 
 <div class="panel panel-default">
-	<div class="panel-heading">
-		<h2 class="panel-title"><?=gettext("Netify Agent Provisioning")?></h2>
-	</div>
-	<div class="panel-body">
-		<div class="content table-responsive">
-			<table class="table table-striped table-hover table-condensed">
-				<tr>
-					<th style='text-align: right; vertical-align: middle;'>Provision Code</th>
-					<td><div style="font-family: monospace;"><?=$agent_uuid;?></div></td>
-				</tr>
-				<tr>
-					<th style='text-align: right; vertical-align: middle;'>Status</th>
-					<td id="provision-status"><?=gettext("Loading...");?></td>
-				</tr>
-				<tr>
-					<td colspan="2" style='text-align: right;'>
-						<button id="btn-provision" class="btn btn-success" type="button" title="<?=gettext('Provision Agent on Netify Portal'); ?>">Provision Netify Agent</button>
-					</td>
-				</tr>
-			</table>
-		</div>
-	</div>
+    <div class="panel-heading">
+        <h2 class="panel-title"><?=gettext("Provision Netify Agent")?></h2>
+    </div>
+    <div class="panel-body">
+        <div class="content table-responsive">
+            <div><img style="width: 18em; margin-bottom: 1em;" src="./images/netify.svg"></div>
+            <table class="table table-striped table-condensed" style="margin-bottom: 1em;">
+                <tr>
+                    <th style='text-align: right; vertical-align: middle;'>Provision Code</th>
+                    <td><div style="font-family: monospace;"><?=$agent_uuid;?></div></td>
+                </tr>
+                <tr>
+                    <th id="sink-status" style='text-align: right; vertical-align: middle; width: 30%;'><?=gettext("Sink Status");?></th>
+                    <td>
+                        <div id="sink-warning" class="text-danger" style="display: none; width: 25em;">
+                            <p><?=gettext("Please enable the Netify Agent to report metadata back to the Netify Informatics Cloud Sink Server before attempting to provision.");?></p>
+                        </div>
+                        <div'><button id="btn-sink-enable" class="btn" type="button" title="<?=gettext("Enable/disable access to the Netify Informatics Cloud Sink Server.");?>" disabled>...</button></div>
+                    </td>
+                </tr>
+                <tr>
+                    <th style='text-align: right; vertical-align: middle;'>Status</th>
+                    <td id="provision-status"><?=gettext("Loading...");?></td>
+                </tr>
+            </table>
+            <span style="float: right;">
+                <button id="btn-provision" class="btn btn-success" type="button" title="<?=gettext('Provision Agent on Netify Portal'); ?>">Provision Netify Agent</button>
+            </span>
+        </div>
+    </div>
 </div>
 
 <script type="text/javascript">
 //<![CDATA[
-	function init() {
-		$(document).ready(function() {
-			console.log('DOM ready.');
-			$('#btn-provision').click(function() {
-				console.log('Provision clicked.');
-				window.open('http://netify.ai/get-netify');
-				return false;
-			});
-		});
-	}
+    var sinkEnabled = false;
 
-	function statusRequest() {
+    function init() {
+        $(document).ready(function() {
+            console.log('DOM ready.');
+            $('#btn-provision').click(function() {
+                console.log('Provision clicked.');
+                window.open('http://netify.ai/get-netify');
+                return false;
+            });
+            $('#btn-sink-enable').click(function() {
+                console.log('Enable/disable sink server.');
+                sinkEnable(! sinkEnabled);
+            });
+        });
+    }
 
-		$.ajax(
-			"<?=$agent_status_url;?>",
-			{
-				type: 'get',
-				success: agentProvisionUpdate,
-				complete: function() {
-					setTimeout(statusRequest, 8000);
-				}
-			}
-		);
-	}
+    function statusRequest() {
 
-	function agentProvisionUpdate(responseData) {
-		console.log('agentProvisionUpdate:');
-		console.log(responseData);
+        $.ajax(
+            "<?=$agent_status_url;?>",
+            {
+                type: 'get',
+                success: agentProvisionUpdate,
+                complete: function() {
+                    setTimeout(statusRequest, 8000);
+                }
+            }
+        );
+    }
 
-		if (responseData.status_code != 0) {
-			$('#provision-status').html(responseData.status_message);
-			$('#provision-status').addClass('text-danger');
-			$('#provision-status').removeClass('text-success');
-		}
-		else if (responseData.data['provisioned'])
-			$('#provision-status').html('Provisioned');
-		else
-			$('#provision-status').html('Not provisioned');
+    function sinkRequest() {
 
-		$('#provision-status').addClass(
-			responseData.data['provisioned'] ? 'text-success' : 'text-danger'
-		);
-		$('#provision-status').removeClass(
-			responseData.data['provisioned'] ? 'text-danger' : 'text-success'
-		);
-	}
+        $.ajax(
+            "<?=$_SERVER['SCRIPT_NAME'];?>",
+            {
+                type: 'post',
+                data: {
+                    status: 'update'
+                },
+                success: agentSinkUpdate,
+                complete: function() {
+                    setTimeout(sinkRequest, 2000);
+                }
+            }
+        );
+    }
 
-	setTimeout(init, 500);
-	setTimeout(statusRequest, 1000);
+    function sinkEnable(enable) {
+
+        $.ajax(
+            "<?=$_SERVER['SCRIPT_NAME'];?>",
+            {
+                type: 'post',
+                data: {
+                    sinkEnable: enable
+                }
+            }
+        );
+    }
+
+    function agentProvisionUpdate(responseData) {
+        console.log('agentProvisionUpdate:');
+        console.log(responseData);
+
+        if (responseData.status_code != 0) {
+            $('#provision-status').html(responseData.status_message);
+            $('#provision-status').addClass('text-danger');
+            $('#provision-status').removeClass('text-success');
+        }
+        else if (responseData.data['provisioned'])
+            $('#provision-status').html('Provisioned');
+        else
+            $('#provision-status').html('Not provisioned');
+
+        $('#provision-status').addClass(
+            responseData.data['provisioned'] ? 'text-success' : 'text-danger'
+        );
+        $('#provision-status').removeClass(
+            responseData.data['provisioned'] ? 'text-danger' : 'text-success'
+        );
+    }
+
+    function agentSinkUpdate(responseData) {
+        console.log('agentSinkUpdate:');
+        console.log(responseData);
+
+        sinkEnabled = responseData['sink_enabled']
+
+        $('#btn-sink-enable').prop('disabled', false);
+
+        if (sinkEnabled) {
+            $('#btn-sink-enable').html('Disable');
+            $('#btn-sink-enable').addClass('btn-danger');
+            $('#btn-sink-enable').removeClass('btn-success');
+            $('#sink-warning').hide();
+            $('#sink-status').html('Sink Enabled');
+        }
+        else {
+            $('#btn-sink-enable').html('Enable');
+            $('#btn-sink-enable').addClass('btn-success');
+            $('#btn-sink-enable').removeClass('btn-danger');
+            $('#sink-warning').show();
+            $('#sink-status').html('Sink Disabled');
+        }
+    }
+
+    setTimeout(init, 500);
+    setTimeout(statusRequest, 1000);
+    setTimeout(sinkRequest, 1000);
 //]]>
 </script>
 
--- deploy/pfsense/files/usr/local/www/netify/netify_status.php
+++ deploy/pfsense/files/usr/local/www/netify/netify_status.php
@@ -3,91 +3,95 @@ require_once('guiconfig.inc');
 require_once('/usr/local/pkg/netify/netify.inc');
 
 if ($_POST['status'] == 'update') {
-	$status = array(
-		'version' => NETIFYD_VERSION,
-		'running' => netifyd_is_running(),
-		'status' => array()
-	);
+    $status = array(
+        'version' => NETIFYD_VERSION,
+        'running' => netifyd_is_running(),
+        'status' => array()
+    );
 
-	if (file_exists(NETIFYD_JSON_STATUS)) {
-		$status['status'] = json_decode(
-			file_get_contents(NETIFYD_JSON_STATUS)
-		);
+    if (file_exists(NETIFYD_JSON_STATUS)) {
+        $status['status'] = json_decode(
+            file_get_contents(NETIFYD_JSON_STATUS)
+        );
 
-		if ($status['status']->sink_status)
-			$status['status']->sink_status = gettext('Yes');
-		else
-			$status['status']->sink_status = gettext('No');
+        if ($status['status']->sink_status) {
+            $status['status']->sink_status_text = gettext('Yes');
 
-		if ($status['status']->dhc_status)
-			$status['status']->dhc_status = gettext('Enabled');
-		else
-			$status['status']->dhc_status = gettext('Disabled');
+            switch ($status['status']->sink_resp_code) {
+            case 1:
+                $resp_text = sprintf('%s (%d)',
+                    gettext('Ok'),
+                    $status['status']->sink_resp_code);
+                break;
+            case 2:
+                $resp_text = sprintf('%s (%d)',
+                    gettext('Authorization failure'),
+                    $status['status']->sink_resp_code);
+                break;
+            case 3:
+                $resp_text = sprintf('%s (%d)',
+                    gettext('Malformed update data'),
+                    $status['status']->sink_resp_code);
+                break;
+            case 4:
+                $resp_text = sprintf('%s (%d)',
+                    gettext('Server error'),
+                    $status['status']->sink_resp_code);
+                break;
+            case 5:
+                $resp_text = sprintf('%s (%d)',
+                    gettext('Upload error'),
+                    $status['status']->sink_resp_code);
+                break;
+            case 6:
+                $resp_text = sprintf('%s (%d)',
+                    gettext('Parse error'),
+                    $status['status']->sink_resp_code);
+                break;
+            case 7:
+                $resp_text = sprintf('%s (%d)',
+                    gettext('Invalid response'),
+                    $status['status']->sink_resp_code);
+                break;
+            case 7:
+                $resp_text = sprintf('%s (%d)',
+                    gettext('Invalid response content type'),
+                    $status['status']->sink_resp_code);
+                break;
+            default:
+                $resp_text = sprintf('%s (%d)',
+                    gettext('Unknown'),
+                    $status['status']->sink_resp_code);
+            }
 
-		switch ($status['status']->sink_resp_code) {
-		case 1:
-			$resp_text = sprintf('%s (%d)',
-				gettext('Ok'),
-				$status['status']->sink_resp_code);
-			break;
-		case 2:
-			$resp_text = sprintf('%s (%d)',
-				gettext('Authorization failure'),
-				$status['status']->sink_resp_code);
-			break;
-		case 3:
-			$resp_text = sprintf('%s (%d)',
-				gettext('Malformed update data'),
-				$status['status']->sink_resp_code);
-			break;
-		case 4:
-			$resp_text = sprintf('%s (%d)',
-				gettext('Server error'),
-				$status['status']->sink_resp_code);
-			break;
-		case 5:
-			$resp_text = sprintf('%s (%d)',
-				gettext('Upload error'),
-				$status['status']->sink_resp_code);
-			break;
-		case 6:
-			$resp_text = sprintf('%s (%d)',
-				gettext('Parse error'),
-				$status['status']->sink_resp_code);
-			break;
-		case 7:
-			$resp_text = sprintf('%s (%d)',
-				gettext('Invalid response'),
-				$status['status']->sink_resp_code);
-			break;
-		case 7:
-			$resp_text = sprintf('%s (%d)',
-				gettext('Invalid response content type'),
-				$status['status']->sink_resp_code);
-			break;
-		default:
-			$resp_text = sprintf('%s (%d)',
-				gettext('Unknown'),
-				$status['status']->sink_resp_code);
-		}
+            $status['status']->sink_resp_code = $resp_text;
+        }
+        else {
+            $status['status']->sink_status_text = gettext('No');
+            $status['status']->sink_resp_code = gettext('Disabled');
+        }
 
-		$status['status']->sink_resp_code = $resp_text;
-	}
-	else {
-		$status['error'] = 'Agent status file not found.';
-	}
+        if ($status['status']->dhc_status)
+            $status['status']->dhc_status = gettext('Enabled');
+        else
+            $status['status']->dhc_status = gettext('Disabled');
+    }
+    else {
+        $status['error'] = 'Agent status file not found.';
+    }
 
-	$response = json_encode($status);
-	header('Content-Type: application/json');
-	header('Content-Length: ' . strlen($response));
+    $response = json_encode($status);
+    header('Content-Type: application/json');
+    header('Content-Length: ' . strlen($response));
 
-	echo json_encode($status);
+    echo json_encode($status);
 
-	exit;
+    exit;
 }
 
-include("head.inc");
 $pgtitle = array(gettext('Services'), gettext('Netify'), gettext('Status'));
+
+include("head.inc");
 
 $tab_array = array();
 $tab_array[] = array(gettext("Status"), true, "/netify/netify_status.php");
@@ -98,185 +102,196 @@ display_top_tabs($tab_array, true);
 ?>
 
 <div class="panel panel-default">
-	<div class="panel-heading">
-		<h2 class="panel-title"><?=gettext("Netify Agent Status")?></h2>
-	</div>
-	<div class="panel-body">
-		<div class="content table-responsive">
-			<table id="maintable" class="table table-striped table-hover table-condensed">
-				<tr>
-					<th>Version</th>
-					<td id="agent_version"></td>
-					<th>Status</th>
-					<td id="agent_status">Unknown</td>
-				</tr>
-				<tr>
-					<th>Last Update</th>
-					<td id="agent_timestamp"></td>
-					<th>Uptime</th>
-					<td id="agent_uptime"></td>
-				</tr>
-				<tr>
-					<th>Sink Enabled</th>
-					<td id="agent_sink_status"></td>
-					<th>Sink Status</th>
-					<td id="agent_sink_resp_code"></td>
-				</tr>
-				<tr>
-					<th>Sink Queue Size</th>
-					<td id="agent_sink_queue_size"></td>
-					<th>Sink Queue Maximum Size</th>
-					<td id="agent_sink_queue_max_size"></td>
-				</tr>
-				<tr>
-					<th>Flows</th>
-					<td id="agent_flows"></td>
-					<th>Flows Delta</th>
-					<td id="agent_flows_delta"></td>
-				</tr>
-				<tr>
-					<th>DNS Hint Caching</th>
-					<td id="agent_dhc_status"></td>
-					<th>DNS Hint Cache Entries</th>
-					<td id="agent_dhc_size"></td>
-				</tr>
-				<tr>
-					<th>Maximum RSS Memory</th>
-					<td id="agent_maxrss"></td>
-					<th>RSS Delta</th>
-					<td id="agent_maxrss_delta"></td>
-				</tr>
-			</table>
-		</div>
-	</div>
+    <div class="panel-heading">
+        <h2 class="panel-title"><?=gettext("Netify Agent Status")?></h2>
+    </div>
+    <div class="panel-body">
+        <div class="content table-responsive">
+            <table id="maintable" class="table table-striped table-hover table-condensed">
+                <tr>
+                    <th>Version</th>
+                    <td id="agent_version"></td>
+                    <th>Status</th>
+                    <td id="agent_status">Unknown</td>
+                </tr>
+                <tr>
+                    <th>Last Update</th>
+                    <td id="agent_timestamp"></td>
+                    <th>Uptime</th>
+                    <td id="agent_uptime"></td>
+                </tr>
+                <tr>
+                    <th>Sink Enabled</th>
+                    <td id="agent_sink_status"></td>
+                    <th>Sink Status</th>
+                    <td id="agent_sink_resp_code"></td>
+                </tr>
+                <tr id="agent_sink_queue" style="visibility: collapse;">
+                    <th>Sink Queue Size</th>
+                    <td id="agent_sink_queue_size"></td>
+                    <th>Sink Queue Maximum Size</th>
+                    <td id="agent_sink_queue_max_size"></td>
+                </tr>
+                <tr>
+                    <th>Flows</th>
+                    <td id="agent_flows"></td>
+                    <th>Flows Delta</th>
+                    <td id="agent_flows_delta"></td>
+                </tr>
+                <tr>
+                    <th>DNS Hint Caching</th>
+                    <td id="agent_dhc_status"></td>
+                    <th>DNS Hint Cache Entries</th>
+                    <td id="agent_dhc_size"></td>
+                </tr>
+                <tr>
+                    <th>Maximum RSS Memory</th>
+                    <td id="agent_maxrss"></td>
+                    <th>RSS Delta</th>
+                    <td id="agent_maxrss_delta"></td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </div>
 
 <script type="text/javascript">
 //<![CDATA[
 
-	function statusRequest() {
+    function statusRequest() {
 
-		$.ajax(
-			"<?=$_SERVER['SCRIPT_NAME'];?>",
-			{
-				type: 'post',
-				data: {
-					status: 'update'
-				},
-				success: statusUpdate,
-				complete: function() {
-					setTimeout(statusRequest, 2000);
-				}
-			}
-		);
-	}
+        $.ajax(
+            "<?=$_SERVER['SCRIPT_NAME'];?>",
+            {
+                type: 'post',
+                data: {
+                    status: 'update'
+                },
+                success: statusUpdate,
+                complete: function() {
+                    setTimeout(statusRequest, 2000);
+                }
+            }
+        );
+    }
 
-	function uptime(seconds) {
-		var days = 0, hours = 0, minutes = 0;
+    function uptime(seconds) {
+        var days = 0, hours = 0, minutes = 0;
 
-		if (seconds >= 86400) {
-			days = Math.floor(seconds / 86400);
-			seconds -= days * 86400;
-		}
+        if (seconds >= 86400) {
+            days = Math.floor(seconds / 86400);
+            seconds -= days * 86400;
+        }
 
-		if (seconds >= 3600) {
-			hours = Math.floor(seconds / 3600);
-			seconds -= hours * 3600;
-		}
+        if (seconds >= 3600) {
+            hours = Math.floor(seconds / 3600);
+            seconds -= hours * 3600;
+        }
 
-		if (seconds >= 60) {
-			minutes = Math.floor(seconds / 60);
-			seconds -= minutes * 60;
-		}
+        if (seconds >= 60) {
+            minutes = Math.floor(seconds / 60);
+            seconds -= minutes * 60;
+        }
 
-		return days.toString().padStart(2, '0') + 'd ' +
-			hours.toString().padStart(2, '0') + ':' +
-			minutes.toString().padStart(2, '0') + ':' +
-			seconds.toString().padStart(2, '0');
-	}
+        return days + 'd ' +
+            hours.toString().padStart(2, '0') + ':' +
+            minutes.toString().padStart(2, '0') + ':' +
+            seconds.toString().padStart(2, '0');
+    }
 
-	function statusUpdate(responseData) {
-		/*
-		{
-		  "type": "agent_status",
-		  "timestamp": 1573494902,
-		  "uptime": 540,
-		  "flows": 55,
-		  "flows_prev": 35,
-		  "maxrss_kb": 42308,
-		  "maxrss_kb_prev": 42300,
-		  "dhc_status": true,
-		  "dhc_size": 16,
-		  "sink_status": true,
-		  "sink_queue_size_kb": 0,
-		  "sink_queue_max_size_kb": 2048,
-		  "sink_resp_code": 1
-		}
-		*/
-		console.log('statusUpdate:');
+    function statusUpdate(responseData) {
+        /*
+        {
+          "type": "agent_status",
+          "timestamp": 1573494902,
+          "uptime": 540,
+          "flows": 55,
+          "flows_prev": 35,
+          "maxrss_kb": 42308,
+          "maxrss_kb_prev": 42300,
+          "dhc_status": true,
+          "dhc_size": 16,
+          "sink_status": true,
+          "sink_queue_size_kb": 0,
+          "sink_queue_max_size_kb": 2048,
+          "sink_resp_code": 1
+        }
+        */
+        console.log('statusUpdate:');
 
-		for(var key in responseData.status) {
-			console.log(
-				'key: ' + key +
-				', value: ' + responseData.status[key]
-			);
-		}
+        for(var key in responseData.status) {
+            console.log(
+                'key: ' + key +
+                ', value: ' + responseData.status[key]
+            );
+        }
 
-		$('#agent_version').html('v' + responseData.version);
-		$('#agent_status').html(responseData.running ? 'Running' : 'Stopped');
-		$('#agent_status').addClass(
-			responseData.running ? 'text-success' : 'text-danger'
-		);
-		$('#agent_status').removeClass(
-			responseData.running ? 'text-danger' : 'text-success'
-		);
-		var timestamp = new Date(responseData.status['timestamp'] * 1000);
-		$('#agent_timestamp').html(timestamp.toLocaleString());
-		$('#agent_uptime').html(uptime(responseData.status['uptime']));
-		$('#agent_sink_status').html(responseData.status['sink_status']);
-		$('#agent_sink_resp_code').html(responseData.status['sink_resp_code']);
-		var sink_queue_percentage =
-			responseData.status['sink_queue_size_kb'] * 100 /
-			responseData.status['sink_queue_max_size_kb'];
-		var sink_queue_percentage_options = {
-			'style': 'percent',
-			'minimumFractionDigits': 2,
-			'maximumFractionDigits': 2
-		};
-		$('#agent_sink_queue_size').html(
-			responseData.status['sink_queue_size_kb'].toLocaleString() + ' kB (' +
-			sink_queue_percentage.toLocaleString('en-US',
-				sink_queue_percentage_options) + ')'
-		);
-		$('#agent_sink_queue_size').addClass(
-			sink_queue_percentage < 50 ? 'text-success' : 'text-danger'
-		);
-		$('#agent_sink_queue_size').removeClass(
-			sink_queue_percentage >= 50 ? 'text-success' : 'text-danger'
-		);
-		$('#agent_sink_queue_max_size').html(
-			responseData.status['sink_queue_max_size_kb'].toLocaleString() + ' kB'
-		);
-		$('#agent_flows').html(responseData.status['flows'].toLocaleString());
-		$('#agent_flows_delta').html(
-			(responseData.status['flows'] - 
-			responseData.status['flows_prev']).toLocaleString()
-		);
-		$('#agent_maxrss').html(
-			responseData.status['maxrss_kb'].toLocaleString() + ' kB'
-		);
-		$('#agent_maxrss_delta').html(
-			(responseData.status['maxrss_kb'] - 
-			responseData.status['maxrss_kb_prev']).toLocaleString() +
-			' kB'
-		);
-		$('#agent_dhc_status').html(responseData.status['dhc_status']);
-		$('#agent_dhc_size').html(
-			responseData.status['dhc_size'].toLocaleString()
-		);
-	}
+        $('#agent_version').html('v' + responseData.version);
+        $('#agent_status').html(responseData.running ? 'Running' : 'Stopped');
+        $('#agent_status').addClass(
+            responseData.running ? 'text-success' : 'text-danger'
+        );
+        $('#agent_status').removeClass(
+            responseData.running ? 'text-danger' : 'text-success'
+        );
+        var timestamp = new Date(responseData.status['timestamp'] * 1000);
+        $('#agent_timestamp').html(timestamp.toLocaleString());
+        $('#agent_uptime').html(uptime(responseData.status['uptime']));
+        $('#agent_sink_status').html(responseData.status['sink_status_text']);
+        $('#agent_sink_resp_code').html(responseData.status['sink_resp_code']);
 
-	setTimeout(statusRequest, 1000);
+        if (responseData.status['sink_status']) {
+            $('#agent_sink_queue').css("visibility", "visible");
+
+            var sink_queue_percentage =
+                responseData.status['sink_queue_size_kb'] * 100 /
+                responseData.status['sink_queue_max_size_kb'];
+            var sink_queue_percentage_options = {
+                'style': 'percent',
+                'minimumFractionDigits': 2,
+                'maximumFractionDigits': 2
+            };
+            $('#agent_sink_queue_size').html(
+                responseData.status['sink_queue_size_kb'].toLocaleString() + ' kB (' +
+                sink_queue_percentage.toLocaleString('en-US',
+                    sink_queue_percentage_options) + ')'
+            );
+            $('#agent_sink_queue_size').addClass(
+                sink_queue_percentage < 50 ? 'text-success' : 'text-danger'
+            );
+            $('#agent_sink_queue_size').removeClass(
+                sink_queue_percentage >= 50 ? 'text-success' : 'text-danger'
+            );
+            $('#agent_sink_queue_max_size').html(
+                responseData.status['sink_queue_max_size_kb'].toLocaleString() + ' kB'
+            );
+        }
+        else {
+            $('#agent_sink_queue').css("visibility", "collapse");
+            $('#agent_sink_queue_size').html("0");
+            $('#agent_sink_queue_max_size').html("0");
+        }
+
+        $('#agent_flows').html(responseData.status['flows'].toLocaleString());
+        $('#agent_flows_delta').html(
+            (responseData.status['flows'] -
+            responseData.status['flows_prev']).toLocaleString()
+        );
+        $('#agent_maxrss').html(
+            responseData.status['maxrss_kb'].toLocaleString() + ' kB'
+        );
+        $('#agent_maxrss_delta').html(
+            (responseData.status['maxrss_kb'] -
+            responseData.status['maxrss_kb_prev']).toLocaleString() +
+            ' kB'
+        );
+        $('#agent_dhc_status').html(responseData.status['dhc_status']);
+        $('#agent_dhc_size').html(
+            responseData.status['dhc_size'].toLocaleString()
+        );
+    }
+
+    setTimeout(statusRequest, 1000);
 //]]>
 </script>
 
