4837571b8cb0c3f2c452de2d5000dcf10e2c0a01
Darryl Sokoloski <darryl@sokoloski.ca>
2019-11-26 23:40:33 -0500

    Blur focus on load.

--- deploy/pfsense/files/usr/local/www/netify/netify_provision.php
+++ deploy/pfsense/files/usr/local/www/netify/netify_provision.php
@@ -100,6 +100,7 @@ $agent_status_url = netifyd_get_agent_status_url();
                 copyToClipboard("<?=$agent_uuid;?>");
                 return true;
             });
+	    $('#btn-copy-code').blur();
         });
     }
 
