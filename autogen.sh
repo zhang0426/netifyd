d12a151d8581ccdf5261458a7757136cfd5abf01
Darryl Sokoloski <darryl@sokoloski.ca>
2019-12-10 21:44:12 -0500

    Automatically use bundled gperftools/libtcmalloc when not available.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -x
 
+SUBDIRS="libs/gperftools libs/libmnl libs/libnetfilter-conntrack libs/libnfnetlink libs/ndpi"
+
 # Regenerate configuration files
-(cd ./libs/ndpi/ && ./autogen.sh)
+for d in $SUBDIRS; do
+    (cd $d && ./autogen.sh)
+done
+
 find $(pwd) -name configure.ac | xargs touch
 mkdir -vp m4 libs/inih/m4 || exit 1
 
